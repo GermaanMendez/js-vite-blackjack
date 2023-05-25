f001f7d454924e5b2e9909d4e74a857fcf0bcabf
German Mendez <mendezgerman678@gmail.com>
2023-05-25 20:35:05 -0300

    vit.config.added

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base:'/js-vite-blackjack/'
+
+
+
+})
+//esto lo hago para que mi sitio se ejecute en esta url como base ya que el resto de carpetas/elementos
+//se encutnran dentro de esta carpeta por lo tanto declaro a este como la ruta base de mi app par que funcione
+//por lo tanto da igual si lo corro de manera local o desplegado en un sitio la ruta siempre sera esa
